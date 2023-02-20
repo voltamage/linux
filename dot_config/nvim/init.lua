a458229c58b76b0cf9ffa8255e9b2ea87304e746
voltamage <37029882+voltamage@users.noreply.github.com>
2023-02-19 19:03:52 -0800

    set leader and bootstrapped lazy neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,1 +1,14 @@
--- neovim configuration
+vim.g.mapleader = ' ' -- set <space> as leader key
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim' -- lazy plugin manager bootstrap
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
