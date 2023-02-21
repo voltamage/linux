c1b75b151ca865953c13750883e2bc097ba6c387
voltamage <37029882+voltamage@users.noreply.github.com>
2023-02-20 21:31:48 -0800

    added octo.nvim for github integration

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -52,6 +52,15 @@ require('lazy').setup({ -- activate lazy plugin manager
       'nvim-treesitter/nvim-treesitter',
     },
   },
+  {
+    'pwntester/octo.nvim',
+    config = true, -- run require('gitsigns').setup()
+    dependencies = { -- including github-cli
+      'nvim-tree/nvim-web-devicons',
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     config = function()
