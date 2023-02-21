e314e95b9ddc17ccfa2cec93ad2aecd7bc8bd7b9
voltamage <37029882+voltamage@users.noreply.github.com>
2023-02-21 00:40:05 -0800

    added rainbow delimiters

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -74,6 +74,21 @@ require('lazy').setup({ -- activate lazy plugin manager
     end,
   },
   {
+    'HiPhish/nvim-ts-rainbow2',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        rainbow = {
+          enable = true,
+          query = 'rainbow-parens',
+          strategy = require 'ts-rainbow.strategy.global',
+        },
+      }
+    end,
+  },
+  {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v1.x',
     dependencies = {
